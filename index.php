fb533543ab960930251d43c5f438188a4406dcb5
mariamandour <ndourmaria06@gmail.com>
2026-06-30 23:08:11 +0000

    enregistrer une nouvelle categorie

--- index.php
+++ index.php
@@ -42,3 +42,58 @@ $categories = [
          echo $categorie["nom"]."\n";
     }
  }
+
+
+
+
+
+
+
+ //3: enregistrer une nouvelle categorie
+
+    $codeIsValid = true;
+    
+   do { 
+        
+        $code = readline("saisir le code :");
+        if (empty($code)) {
+            echo "le code est obligatoire \n";
+             $codeIsValid = false;
+        }else{
+            foreach ($categories as  $categorie ) {
+               if (($categorie["code"]) === $code) {
+                $codeIsValid = false;
+                echo "le code existe deja ...\n"; 
+         }
+       }  
+}
+        
+
+    } while (!$codeIsValid);
+    
+     $nomIsValid = true;
+  do { 
+        
+        $nom = readline("saisir le nom : ");
+        if (empty($nom)) {
+            echo "le nom est obligatoire";
+             $nomIsValid= false;
+        }else{
+            foreach ($categories as  $categorie ) {
+               if (($categorie["nom"]) === $nom) {
+                $nomIsValid = false;
+                echo "le nom existe deja ..."; 
+         }
+       }  
+}
+    } while (!$nomIsValid);
+
+
+
+    $categorie  =   [
+            "code" => $code,
+            "nom" => $nom,
+            "produits" => []
+         ];
+
+         $categories[] = $categorie;
