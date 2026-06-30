f711228ec4811e5e2f0cd5682e2d968f926a34a2
mariamandour <ndourmaria06@gmail.com>
2026-06-30 23:35:57 +0000

    affichages categorie qui n'ont pas de produits

--- index.php
+++ index.php
@@ -28,3 +28,18 @@ $categories = [
             "produits" => []
          ]
 ];
+
+
+
+
+
+//2:affichages categorie qui n'ont pas de produits
+
+ function afficheCategorieSansProduit(array $categories): void{
+    foreach ($categories as  $categorie ) {
+        if (empty($categorie["produits"])) {
+            echo $categorie["nom"]."\n";
+        }
+    }
+ }
+ afficheCategorieSansProduit($categories);
