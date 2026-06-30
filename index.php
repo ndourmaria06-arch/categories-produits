014ecd1263eafe13e46a96a42aadb54d1312b23c
mariamandour <ndourmaria06@gmail.com>
2026-06-30 22:49:07 +0000

    initialisation-tableau

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+// 1 
+
+$categories = [
+
+   0 =>      [
+            "code" => "9870",
+            "nom" => "categorie0",
+            "produits" => [
+                  0 => [
+                    "nom" => "produit1",
+                    "reference" => "ref1",
+                    "prix" => 3000,
+                    "quantite" => 10
+                  ],
+                  1 => [
+                    "nom" => "produit2",
+                    "reference" => "ref2",
+                    "prix" => 2000,
+                    "quantite" => 15
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "coo1",
+            "nom" => "categorie1",
+            "produits" => []
+         ]
+];
